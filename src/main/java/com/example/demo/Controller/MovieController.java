8e7ee0a9158dc7b5ba1997b1ce1c7f7d7cd25079
baquy111103 <145130742+baquy111103@users.noreply.github.com>
2024-12-25 09:26:42 +0700

    typre

--- src/main/java/com/example/demo/Controller/MovieController.java
+++ src/main/java/com/example/demo/Controller/MovieController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,5 +20,12 @@ public class MovieController {
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies() {
         return ResponseEntity.ok(movieService.getAllMovies());
+    }
+
+    @GetMapping("/type")
+    public ResponseEntity<List<Movie>> getMoviesByType(@RequestParam Boolean type) {
+        List<Movie> movies = movieService.getMoviesByType(type);
+
+        return ResponseEntity.ok(movies);
     }
 }
